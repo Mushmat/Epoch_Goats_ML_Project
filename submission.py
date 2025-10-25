2f9406a79cf0649ece13883dda35ff47eaf90c7b
Mushmat <edu.chirayu2005@gmail.com>
2025-10-25 17:19:58 +0530

    first try of submission

--- submission.py
+++ submission.py
@@ -1,15 +1,16 @@
-# submission.py
 import pandas as pd
 import joblib
-from sklearn.pipeline import Pipeline
 import config
 
 def make_submission(model_path, test_path, submit_path="my_submission.csv"):
     test_df = pd.read_csv(test_path)
     model = joblib.load(model_path)
-    preds = model.predict(test_df.drop(columns=[config.ID_COL]))
-    submission = pd.read_csv(config.SUBMISSION_SAMPLE)
-    submission[config.TARGET] = preds
+    X_test = test_df.drop(columns=[config.ID_COL])
+    preds = model.predict(X_test)
+    submission = pd.DataFrame({
+        config.ID_COL: test_df[config.ID_COL],
+        config.TARGET: preds
+    })
     submission.to_csv(submit_path, index=False)
     print("Submission saved:", submit_path)
 
